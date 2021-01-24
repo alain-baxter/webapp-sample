3c4b700ea60e31f15a9cbf771de5073d105d029a
root <root@AlainsDesktop.localdomain>
2021-01-24 16:34:28 -0500

    Test listener Post Change

--- update-image.sh
+++ update-image.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+
 # Get Args
 IMAGE_NAME=`echo "$1" | tr -d '\r'`
 IMAGE_DIGEST=`echo "$2" | tr -d '\r'`
+
 # Replace image tag in deployment
 sed -ri "s#^(\s*)(image\s*:.*$)#\1image: ${IMAGE_NAME}@${IMAGE_DIGEST}#" config/base/deployment.yaml
